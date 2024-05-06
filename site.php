d4e252700f02232bf5b360d317ad563293b42fa1
Mercy Oyelude <mercyoyelude0@gmail.com>
2024-05-06 16:48:19 +0100

    cakephp mvp project.

---
+++ site.php
@@ -0,0 +1,19 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>cakephp</title>
+    <link rel="stylesheet" href="style.css">
+</head>
+<body>
+    
+    <form action="site.php" method="post">
+       
+    </form>
+    <?php include "header.html"    ?>
+    <?php include "plaque.html"    ?>
+    <?php include "form.html"    ?>
+    <?php include "footer.html"    ?> 
+</body>
+</html>
