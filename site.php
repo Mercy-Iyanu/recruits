87c97b831ff136b138b7a33149653fa301e6e870
Mercy Oyelude <mercyoyelude0@gmail.com>
2024-05-06 20:23:36 +0100

    Updated.

--- site.php
+++ site.php
@@ -4,16 +4,13 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>cakephp</title>
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css" rel="stylesheet">
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/js/bootstrap.bundle.min.js"></script>
     <link rel="stylesheet" href="style.css">
 </head>
-<body>
-    
-    <form action="site.php" method="post">
-       
-    </form>
+<body class="container-fluid">
     <?php include "header.html"    ?>
-    <?php include "plaque.html"    ?>
-    <?php include "form.html"    ?>
+    <?php include "body.html"    ?>
     <?php include "footer.html"    ?> 
 </body>
 </html>
